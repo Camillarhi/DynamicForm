e2f90e5fe5a394f207e5a04c86e3677e8cf5f22e
Camillarhi <rhitanene@gmail.com>
2024-05-17 11:20:15 +0100

    configure auto mapper

---
+++ DynamicForm/Configurations/MapperInitializer.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using DynamicForm.DTOs;
+using DynamicForm.Models;
+
+namespace DynamicForm.Configurations
+{
+    public class MapperInitializer : Profile
+    {
+        public MapperInitializer()
+        {
+            CreateMap<ProgramConfiguration, ProgramConfigurationDTO>().ReverseMap();
+            CreateMap<ProgramConfiguration, CreateProgramConfigurationDTO>().ReverseMap();
+            CreateMap<ApplicationFormConfiguration, ApplicationFormConfigurationDTO>().ReverseMap();
+            CreateMap<ApplicationFormConfiguration, CreateApplicationFormConfigurationDTO>().ReverseMap();
+            CreateMap<QuestionConfiguration, QuestionConfigurationDTO>().ReverseMap();
+            CreateMap<QuestionConfiguration, CreateQuestionConfigurationDTO>().ReverseMap();
+            CreateMap<FieldConfiguration, FieldConfigurationDTO>().ReverseMap();
+            CreateMap<Choice, ChoiceDTO>().ReverseMap();
+        }
+    }
+}
