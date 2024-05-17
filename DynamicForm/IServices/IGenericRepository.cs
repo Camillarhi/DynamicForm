691f55602e78f0bc204b0d634b4e01791e526b09
Camillarhi <rhitanene@gmail.com>
2024-05-17 11:21:12 +0100

    setup unit of work and generic repository interfaces

---
+++ DynamicForm/IServices/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+
+namespace DynamicForm.IServices
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        public Task<IList<T>> GetAll(
+                    Expression<Func<T, bool>>? expression = null,
+                    Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null
+                    );
+        public Task<T> GetOne(Expression<Func<T, bool>> expression);
+        public Task<ActionResult<T>> Create(T entity);
+        public Task<IActionResult> Update(Guid id, T entity);
+        public Task<IActionResult> Delete(Guid id);
+    }
+}
---
+++ DynamicForm/IServices/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DynamicForm.IServices
+{
+    public interface IUnitOfWork
+    {
+        DbContext Context { get; }
+        Task SaveChangesAsync();
+    }
+}
